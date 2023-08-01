a312292c9021c85e71469aff4537482429492959
Pankaj Bohra <pankaj.bohra@applaunch.io>
2023-08-01 12:52:05 +0530

    Extreme in array

--- DSA_Question/src/dsa/lovebabbar/supreme_1/array_list/ExtremeInArray.java
+++ DSA_Question/src/dsa/lovebabbar/supreme_1/array_list/ExtremeInArray.java
@@ -1,13 +1,21 @@
 package dsa.lovebabbar.supreme_1.array_list;
 
 public class ExtremeInArray {
-    public static void main(String[] args){
-        int[] nums = {1,2,3,4,5,6,7,8};
-        System.out.println(getExtremeArray(nums));
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        getExtremeArray(nums);
     }
 
-    private static int[] getExtremeArray(int [] nums){
-        int [] extremeArray = {};
-        return extremeArray;
+    private static void getExtremeArray(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            System.out.print(nums[start] + " ");
+            System.out.print(nums[end] + " ");
+            start++;
+            end--;
+        }
+
     }
 }
