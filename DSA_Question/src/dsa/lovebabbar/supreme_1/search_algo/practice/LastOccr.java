736a786ae0aeb71f49e4a42d422c1709f6269ee7
pankaj1920 <42286224+pankaj1920@users.noreply.github.com>
2023-08-23 08:18:23 +0530

    Last occurence practice

--- DSA_Question/src/dsa/lovebabbar/supreme_1/search_algo/practice/LastOccr.java
+++ DSA_Question/src/dsa/lovebabbar/supreme_1/search_algo/practice/LastOccr.java
@@ -1,25 +1,35 @@
 package dsa.lovebabbar.supreme_1.search_algo.practice;
 
 public class LastOccr {
-    public static void main (String[] args){
-        int[] arr = {1, 3, 4, 4, 4, 4, 4, 6, 7, 9};
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 4, 4, 4, 4, 6, 7, 7,9};
         int target = 4;
-        int lastAccr = findLastOccur(arr,target);
+        int lastAccr = findLastOccur(arr, 7);
+
+        System.out.println(lastAccr);
     }
 
     private static int findLastOccur(int[] arr, int target) {
+        int ans = -1;
+
         int start = 0;
-        int end = arr.length -1;
-        int mid = start+(end - start)/2;
-        while (start<=end){
-            if (target==arr[mid]){
-                start = mid +1;
+        int end = arr.length - 1;
+        int mid = start + (end - start) / 2;
+
+        while(start<=end){
+
+            if (target == arr[mid]){
+                ans = mid;
+                start = mid+1;
             }else if (target< arr[mid]){
-                mid = end-1;
+                end = mid -1;
             }else{
-                mid = start+1;
+                start = mid+1;
             }
-            mid = start +(end-start)/2;
+            mid = start+(end-start)/2;
         }
+
+
+        return ans;
     }
 }
