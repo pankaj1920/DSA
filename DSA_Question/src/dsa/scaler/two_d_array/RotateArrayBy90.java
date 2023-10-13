35db035e4bbb9aaa6edae159f198ea3cd97bf2b9
pankaj1920 <bohrapankaj1920@gmail.com>
2023-10-13 07:36:53 +0530

    Scaller

---
+++ DSA_Question/src/dsa/scaler/two_d_array/RotateArrayBy90.java
@@ -0,0 +1,14 @@
+package dsa.scaler.two_d_array;
+
+import java.util.Arrays;
+
+public class RotateArrayBy90 {
+    public static void main(String[] args) {
+        int [][] arr = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+        System.out.println(Arrays.deepToString(rotate90(arr)));
+    }
+
+    private static int[][] rotate90(int[][] arr) {
+      
+    }
+}
